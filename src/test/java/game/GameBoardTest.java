1d14d4194104de40ef10b1a3f53ace076f79cf8c
Ahmed elSayed <aelsayed.mo@gmail.com>
2018-06-08 19:23:04 +0200

    moved test file

---
+++ src/test/java/game/GameBoardTest.java
@@ -0,0 +1,12 @@
+package game;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class GameBoardTest {
+  // @Test
+  // public void stateMachineIsSuccessfullyCreated() {
+  // GameBoard gameBoard = new GameBoard();
+  // assertNotNull("State Machine is created.", gameBoard.stateMachine);
+  // }
+}
